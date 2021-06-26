4fc70fb12b8500b208bed29cc1a956b296643d6c
Vish O <v.opatha@gmail.com>
2021-06-26 19:20:02 +0930

    Refactor javascript v1

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,6 @@ var passWord=""; //Initial password
 /*Boolean variables stating if the user needs any type of letters, 
 numbers or special characters */
 var incUpperCase; var incLowerCase; var incNumbers; var incSpecialChar;
-var isLengthCorrect;
 var pwCombination = [0,0,0,0]; //numbers,uppercase,lowercase,special characters
 var numberList=["0","1","2","3","4","5","6","7","8","9"];
 var capLetters=["A","B","C","D","E","F","G","H","I","J","K","L",
@@ -14,10 +13,10 @@ var smallLetters=["a","b","c","d","e","f","g","h","i","j","k","l",
 var specialChar=["!","#","$","%","&","(",")","*","+","-",".","/",":",";","<","=",
                   ">","?","@","[","]","^","_","`","{","|","}","`"];
 var listOfChars; 
-var n=0;
+var pwArrayIndex=0;
 
 var remCharacters=0;
-var multicombination= [];
+var pwArray= [];
 var outerA;
 var innerA;
 var letterss="";
@@ -28,49 +27,31 @@ var generateBtn = document.querySelector('#generate');
 
 // Arrays for capital letters and special characters
 
-function generatePassword(){
-pwLength=window.prompt("Please enter the desired length of your password?");
-
-while(pwLength<8 || pwLength>128)
+function generatePassword()
 {
-  window.alert("Enter a number between 8 to 128!");
-  pwLength=window.prompt("Please enter the desired length of your password?");
-}
+    pwLength=window.prompt("Please enter the desired length of your password?");
+    // Validate the length of the password
+    while(pwLength<8 || pwLength>128)
+    {
+        window.alert("Enter a number between 8 to 128!");
+        pwLength=window.prompt("Please enter the desired length of your password?");
+    }
 
-incNumbers = window.confirm("Do you need numbers in your password?");
-window.alert(incNumbers);
-incUpperCase = window.confirm("Do you need uppercase letters in your password?");
-window.alert(incUpperCase);
-incLowerCase = window.confirm("Do you need lowercase letters in your password?");
-window.alert(incLowerCase);
-incSpecialChar = window.confirm("Do you need special characters in your password?");
-window.alert(incSpecialChar);
+    // Get user input for numbers, uppercase, lowercase and special characters
+    getUserInput();
+    // Validate if the user select one option out of four
+    while(!(incLowerCase || incUpperCase || incNumbers || incSpecialChar))
+    {
+        window.alert("You need to select at lease one from numbers\n uppercase, lowercase and special characters.");
+        getUserInput();
+    }
 
-
-var checkcombo;
-
-while(!(incLowerCase || incUpperCase || incNumbers || incSpecialChar))
-{
-  window.alert("You need to select at lease one from numbers\n uppercase, lowercase and special characters.")
-  incNumbers = window.confirm("Do you need numbers in your password?");
-  // updateCombination(incNumbers,0,numberList);
-  incUpperCase = window.confirm("Do you need uppercase letters in your password?");
-  // updateCombination(incUpperCase,1,capLetters);
-  incLowerCase = window.confirm("Do you need lowercase letters in your password?");
-  // updateCombination(incLowerCase,2,smallLetters);
-  incSpecialChar = window.confirm("Do you need special characters in your password?");
-}
-
-updateCombination(incNumbers,0,numberList);
-updateCombination(incUpperCase,1,capLetters);
-updateCombination(incLowerCase,2,smallLetters);
-updateCombination(incSpecialChar,3,specialChar);
-
-
-
-
-
-
+    // Generate a password combination and a multidimesional array based of user input
+    // Refer to the function for more details
+    updateCombination(incNumbers,0,numberList);
+    updateCombination(incUpperCase,1,capLetters);
+    updateCombination(incLowerCase,2,smallLetters);
+    updateCombination(incSpecialChar,3,specialChar);
 
 if(pwCombination[0]==1){
 var num=Math.floor(Math.random() * 10);
@@ -106,32 +87,42 @@ var innerK, outerK;
 j=0;
 for(i=0;i<actualRemain;i++)
 {
-  innerK=Math.floor(Math.random() * multicombination.length);
-  var lengthsub=multicombination[innerK];
+  innerK=Math.floor(Math.random() * pwArray.length);
+  var lengthsub=pwArray[innerK];
   outerK=Math.floor(Math.random() * lengthsub.length);
-  passWord=passWord+(multicombination[innerK][outerK]); 
+  passWord=passWord+(pwArray[innerK][outerK]); 
 }
 return passWord;
 }
 
+// This function is used to get the user input for password
+function getUserInput()
+{
+  incNumbers = window.confirm("Do you need numbers in your password?");
+  incUpperCase = window.confirm("Do you need uppercase letters in your password?");
+  incLowerCase = window.confirm("Do you need lowercase letters in your password?");
+  incSpecialChar = window.confirm("Do you need special characters in your password?");
+}
+
+/*This function is used to generate a password combination and an array of arrays relating to that combination.
+  example:pwCombination = [0,1,0,1] --> Customer needs NO numbers, YES uppercase, NO lowercase, YES special chars.
+          According to above the pwArray is [[0,capLetters],[1,specialChar]] */
 function updateCombination(isRequired, index, listOfChars)
 {
-  if(isRequired===true){
+  if(isRequired){
     pwCombination[index]=1;
-    multicombination[n]=listOfChars;
-    n++;
+    pwArray[pwArrayIndex]=listOfChars;
+    pwArrayIndex++;
   }
-  
 }
 
-
-// // Write password to the #password input given
-function writePassword() {
-var password = generatePassword();
-var passwordText = document.querySelector('#password');
-
-passwordText.value = password;
+// Write password to the #password input given
+function writePassword() 
+{
+    var password = generatePassword();
+    var passwordText = document.querySelector('#password');
+    passwordText.value = password;
 }
 
-// // Add event listener to generate button
+// Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
