1297946377692b9e6a12c412c0d42c0913cd4d1e
Vish O <v.opatha@gmail.com>
2023-06-01 10:43:07 +0930

    Update : Refactor and organise the funtions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,20 @@
 let passwordLength; 
 let password; 
-let isUpperCaseRequired; 
+let isUpperCaseRequired;
 let isLowerCaseRequired; 
-let isNumbersRequired; 
+let isNumbersRequired; ;
 let isSpecialCharRequired;
 let pwCombination = []; //numbers,uppercase,lowercase,special characters
-let numberList = ["0","1","2","3","4","5","6","7","8","9"];
-let capLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-let smallLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-let specialChar = ["`","~","!","@","#","$","%","^","&","*","(",")","-","_","=","+","[","]","\\","{","}","|",";","\'",":","\"","\,",".","/","<",">","?"];
+const numberList = ["0","1","2","3","4","5","6","7","8","9"];
+const capLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const smallLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const specialChar = ["`","~","!","@","#","$","%","^","&","*","(",")","-","_","=","+","[","]","\\","{","}","|",";","\'",":","\"","\,",".","/","<",">","?"];
 let listOfChars; // This changes to numberList/capLetters/smallLetters/specialChar
 let pwArray = []; // This is an array comprises of other arrays from above, based on user input
 let count; // This counts the number of times if statement in generateFirstChars fn runs
 let generatePasswordBtn = document.querySelector('#generate');
 
 function getPasswordLength() {
-  console.log('getPasswordLength');
   passwordLength = 0;
   passwordLength = window.prompt('Please enter the desired length of your password?');
 
@@ -24,17 +23,9 @@ function getPasswordLength() {
       getPasswordLength();
       return;
   }
-
-  console.log('inside getPasswordLength password length', passwordLength);
 }
 
 function getPasswordCombination() {
-  console.log('getPasswordCombination')
-  // isUpperCaseRequired = false; 
-  // isLowerCaseRequired = false; 
-  // isNumbersRequired = false; 
-  // isSpecialCharRequired = false;
-
   isNumbersRequired = window.confirm('Do you need numbers in your password?');
   isUpperCaseRequired = window.confirm('Do you need uppercase letters in your password?');
   isLowerCaseRequired = window.confirm('Do you need lowercase letters in your password?');
@@ -44,15 +35,12 @@ function getPasswordCombination() {
       window.alert('You need to select at lease one from numbers\n uppercase, lowercase and special characters.');
       getPasswordCombination();
   }
-
-  console.log('inside getPasswordCombination', isNumbersRequired, isUpperCaseRequired, isLowerCaseRequired, isSpecialCharRequired  );
 }
 
 /*  This function is used to generate a password combination and an array of arrays relating to that combination.
     example:pwCombination = [0,1,0,1] --> Customer needs NO numbers, YES uppercase, NO lowercase, YES special chars.
     According to above the pwArray is [[0,capLetters],[1,specialChar]] */
 function generatePasswordCombination(isRequired, index, listOfChars) {
-  console.log('inside generatePasswordCombination');
   if(isRequired)
   {
     pwCombination[index] = 1;
@@ -64,74 +52,56 @@ function generatePasswordCombination(isRequired, index, listOfChars) {
 /*  This function is used to randomlygenerate the first character from each category given by the user and it will
     make sure the password include at least one character from each category that user entered.  */
 function generateFirstChars(aIndex,refArray) {
-  console.log('generateFirstChars');
   if(pwCombination[aIndex] === 1)
   {
-     let index=Math.floor(Math.random() * refArray.length);
-     let firstChar=refArray[index];
-     password = password +''+ firstChar+'';
+     let index = Math.floor(Math.random() * refArray.length);
+     let firstChar = refArray[index];
+
+     password = `${password}${firstChar}`;
      count = count + 1; 
   }
-
-  console.log('inside generateFirstChars, password', password);
 }
 
 // Main function to generate the password, this has multiple functions inside
 function generatepassword() {
-  console.log('inside generatepassword')
-  let remCharacters = passwordLength - count; //actual number of times the below for loop would run
+  const remainingCharacters = passwordLength - count; //actual number of times the below for loop would run
   let rIndex1, rIndex2, randomArray;
-  password = ''; 
-  
-  count = 0; 
-  
-  
-  
-  
-  
-
-  // Generate one character from each category to satisfy the user input - validation 
-  // generateFirstChars(0,numberList); 
-  // generateFirstChars(1,capLetters); 
-  // generateFirstChars(2,smallLetters); 
-  // generateFirstChars(3,specialChar); 
 
   // Generate the remaining number of characters randomly from random arrays 
-  // for(i = 0; i < remCharacters; i++) {
-  //   rIndex1 = Math.floor(Math.random() * pwArray.length); //Pickup any array randomly according to the user input
-  //   randomArray = pwArray[rIndex1];
-  //   rIndex2 = Math.floor(Math.random() * randomArray.length); //Pickup random index for the randomly selected above array
-      
-  //   password = password + (pwArray[rIndex1][rIndex2]); 
-  // }
+  for(i = 0; i < remainingCharacters; i++) {
+    rIndex1 = Math.floor(Math.random() * pwArray.length); //Pickup any array randomly according to the user input
+    randomArray = pwArray[rIndex1];
+    rIndex2 = Math.floor(Math.random() * randomArray.length); //Pickup random index for the randomly selected above array
+    
+    password = `${password}${pwArray[rIndex1][rIndex2]}`
+  }
   
-  console.log('password', password);
   return password;
 }
 
 function displayPassword() 
 {
-    console.log('inside displayPassword')
+    let passwordText = document.querySelector('#password');
     getPasswordLength();
     getPasswordCombination();
     password = '';
     pwCombination = [0,0,0,0];
     pwArrayIndex = 0;
-    pwArray = []; 
+    pwArray = [];
+    count = 0; 
+
     generatePasswordCombination(isNumbersRequired,0,numberList);
     generatePasswordCombination(isUpperCaseRequired,1,capLetters);
     generatePasswordCombination(isLowerCaseRequired,2,smallLetters);
     generatePasswordCombination(isSpecialCharRequired,3,specialChar);
 
-    console.log('========= PW combination', pwCombination);
-    console.log('========= PW character array', pwArray);
+    generateFirstChars(0,numberList); 
+    generateFirstChars(1,capLetters); 
+    generateFirstChars(2,smallLetters); 
+    generateFirstChars(3,specialChar);
 
-    // let password = '';
-    // password = generatepassword();
-    // let passwordText = document.querySelector('#password');
-    // passwordText.textContent = password;
-    // console.log('=========================');
-    // console.log('displayPassword, password');
+    password = generatepassword();
+    passwordText.textContent = password;
 }
 
 // Add event listener to generate button
